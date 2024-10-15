41a798ff8792fb8ff24dfae9470196feb11e076f
Swaraj Bandhu Prasad Kalwar <swarajbandhi0000@gmail.com>
2024-10-15 17:00:57 +0530

    done with some of the crud applications in java eclipse

--- src/test/java/Demo/Delete.java
+++ src/test/java/Demo/Delete.java
@@ -1,22 +1,14 @@
 package Demo;
-
-
 import static io.restassured.RestAssured.*;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-public class practice {
+public class Delete {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Response response = given().auth().basic("admin", "admin")
-				.contentType(ContentType.JSON)
-				.queryParams("id", 6)
-				.get("http://localhost:3000/demo");
-				response.prettyPrint();
+		given().contentType(ContentType.JSON).delete("http://localhost:3000/demo/10");
 
 	}
 
 }
-
